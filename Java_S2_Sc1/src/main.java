5d4e1744a4317391931366ef1db24128de26f630
rbenyoun <rbenyoun@JFC-9V9P8D2>
2023-02-01 11:58:23 +0200

    boutton

--- Java_S2_Sc1/src/main.java
+++ Java_S2_Sc1/src/main.java
@@ -1,9 +1,17 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		 MyFrame mf = new MyFrame("Mon bouton",  Color.pink);
+	   	 MyFrame mf1 = new MyFrame("titre", "Un bouton", Color.green);
+	   	 MyFrame mf2 = new MyFrame("salut");
 
 	}
-
 }
